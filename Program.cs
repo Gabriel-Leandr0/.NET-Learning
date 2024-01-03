397d4ee6964f2b24c97b44522bd6d34d128a3d64
Gabriel <gabriel.leandrods@gmail.com>
2024-01-03 15:09:05 -0300

    Tipagem de dados

--- Program.cs
+++ Program.cs
@@ -4,11 +4,38 @@ public class Program
 {
     static void Main(string[] args)
     {
-        var texto = "Testando";
-        for (int i = 0; i < 10; i++)
-        {
-            Console.WriteLine(texto+" "+i);
-            
-        }
+        //VAR: tipagem definida pelo valor da variavel
+        var nome = "Gabriel";
+        
+        //Strings
+        String nome2 = "Neemias";
+        string nome3 = "Diogo";
+        
+        //int
+        int numero = 0;
+        
+        //float
+        float numero22 = 25.000f;
+        var numero2 = 25.000f;
+        
+        //decimal
+        var numero3 = 25.000m;
+        decimal numero33 = 25.000m;
+        
+        //double
+        double numero44 = 25.000;
+        var numero4 = 25.000;
+        //boolean
+        
+        bool status = false;
+        //char
+        char umaLetra = 'G';
+        
+        //object: podemos utilizar para definir uma tipagem complexa, podemos definir e alterar para qlq valor
+        object naoSeiMinhaTipagem = 123;
+        naoSeiMinhaTipagem = "ALO";
+        
+        //constante: tera o valor fixado do começo ao fim, por padrao utilizar letras maiusculas
+        const string TEXTO_TESTE = "Testando";
     }   
 }
