32a257da484b3c2016cf932019338d7d9a91ee4b
gabriel-leandr0 <gabriel.leandrods@gmail.com>
2024-01-02 22:14:02 -0300

    Criando variavel e imprimindo na tela

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace dotNET_Learning;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        var texto = "Testando";
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine(texto+" "+i);
+            
+        }
+    }   
+}
